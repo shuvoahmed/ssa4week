06ae65f4bfbb989aa26df0fa083565bf0a7a43c2
Shuvo Ahmed <shuvoahmed@hotmail.com>
2016-08-10 08:53:01 -0400

    Start Day 8

--- day7/nodejs/expressexample/express.js
+++ day7/nodejs/expressexample/express.js
@@ -1,7 +1,20 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 app.use(express.static('public'));
+app.use(express.static('files'));
+
+// fs.readFile('./public/images/pokemon1.jpg',(err,data) => {
+//     if (err) {
+//         throw err;
+//     }
+
+//     app.get('/', function(req, res) {
+//         res.send(data);
+//     });
+// });
+
 
 app.get('/', function(req, res) {
     res.send('Hello World!');
